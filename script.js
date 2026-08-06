4726fa93c32366b61655673b74adb6d0aebaa7ed
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-08-06 22:04:45 +0600

    add preloader

--- script.js
+++ script.js
@@ -3,7 +3,22 @@
         const closeMenuButton = document.getElementById("close-menu-button");
         const mobileMenu = document.getElementById("mobile-menu");
 
-        mobileMenuButton.addEventListener("click", function () {
+      // preloader 
+        window.addEventListener("load", () => {
+        const preloader = document.getElementById("preloader");
+
+         if (!preloader) return;
+              setTimeout(() => {
+              preloader.classList.add("opacity-0", "pointer-events-none");
+ 
+               setTimeout(() => {
+                preloader.remove();
+                }, 700); 
+            }, 1000);
+          });
+
+          // mobile menu
+          mobileMenuButton.addEventListener("click", function () {
           mobileMenu.classList.remove("hidden");
           mobileMenu.classList.add("flex");
           document.body.style.overflow = "hidden"; // Prevent scrolling when menu is open
